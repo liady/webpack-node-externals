961be84e03b3cf7b9b940a987b603ab052a17b26
Liad Yosef <liad.yosef@gmail.com>
2016-04-21 01:57:04 +0300

    adjust test configuration

--- test/library.spec.js
+++ test/library.spec.js
@@ -7,17 +7,24 @@ var assertResult = testUtils.buildAssertion.bind(null, context);
 
 // Test basic functionality
 describe('invocation with no settings', function() {
+
     before(function(){
         mockNodeModules();
         context.instance = nodeExternals();
     });
-    it('should invoke a commonjs callback when given an existing module',       assertResult('moduleA', 'commonjs moduleA'));
-    it('should invoke a commonjs callback when given another existing module',  assertResult('moduleB', 'commonjs moduleB'));
-    it('should invoke a commonjs callback when given an existing sub-module',   assertResult('moduleA/sub-module', 'commonjs moduleA/sub-module'));
-    it('should invoke a commonjs callback when given an existing file in a sub-module',  assertResult('moduleA/another-sub/index.js', 'commonjs moduleA/another-sub/index.js'));
 
-    it('should invoke an empty callback when given a non-node module',           assertResult('non-node-module', undefined));
-    it('should invoke an empty callback when given a relative path',             assertResult('./src/index.js', undefined));
+    describe('should invoke a commonjs callback', function(){
+        it('when given an existing module', assertResult('moduleA', 'commonjs moduleA'));
+        it('when given another existing module', assertResult('moduleB', 'commonjs moduleB'));
+        it('when given an existing sub-module', assertResult('moduleA/sub-module', 'commonjs moduleA/sub-module'));
+        it('when given an existing file in a sub-module', assertResult('moduleA/another-sub/index.js', 'commonjs moduleA/another-sub/index.js'));
+    });
+
+    describe('should invoke an empty callback', function(){
+        it('when given a non-node module', assertResult('non-node-module', undefined));
+        it('when given a relative path', assertResult('./src/index.js', undefined));
+    });
+
     after(function(){
         restoreMock()
     });
@@ -25,17 +32,24 @@ describe('invocation with no settings', function() {
 
 // Test different "importType"
 describe('invocation with a different importType', function() {
+
     before(function(){
         mockNodeModules();
         context.instance = nodeExternals({importType: 'var'});
     });
-    it('should invoke a var callback when given an existing module',         assertResult('moduleA', 'var moduleA'));
-    it('should invoke a var callback when given another existing module',    assertResult('moduleB', 'var moduleB'));
-    it('should invoke a var callback when given an existing sub-module',     assertResult('moduleA/sub-module', 'var moduleA/sub-module'));
-    it('should invoke a var callback when given an existing file in a sub-module',  assertResult('moduleA/another-sub/index.js', 'var moduleA/another-sub/index.js'));
 
-    it('should invoke an empty callback when given a non-node module',       assertResult('non-node-module', undefined));
-    it('should invoke an empty callback when given a relative path',         assertResult('./src/index.js', undefined));
+    describe('should invoke a var callback', function(){
+        it('when given an existing module', assertResult('moduleA', 'var moduleA'));
+        it('when given another existing module', assertResult('moduleB', 'var moduleB'));
+        it('when given an existing sub-module', assertResult('moduleA/sub-module', 'var moduleA/sub-module'));
+        it('when given an existing file in a sub-module', assertResult('moduleA/another-sub/index.js', 'var moduleA/another-sub/index.js'));
+    });
+
+    describe('should invoke an empty callback', function(){
+        it('when given a non-node module', assertResult('non-node-module', undefined));
+        it('when given a relative path', assertResult('./src/index.js', undefined));
+    });
+
     after(function(){
         restoreMock()
     });
@@ -43,24 +57,31 @@ describe('invocation with a different importType', function() {
 
 // Test whitelist
 describe('honors a whitelist', function() {
+
     before(function(){
         mockNodeModules();
         context.instance = nodeExternals({
             whitelist: ['moduleA/sub-module', 'moduleA/another-sub/index.js', 'moduleC', /^moduleD/]
         });
     });
-    it('should invoke a commonjs callback when given an existing module',        assertResult('moduleB', 'commonjs moduleB'));
-    it('should invoke a commonjs callback when given an existing sub-module',    assertResult('moduleB/sub-module', 'commonjs moduleB/sub-module'));
-    it('should invoke a commonjs callback when given a module which is the parent on an ignored path',  assertResult('moduleA', 'commonjs moduleA'));
-    it('should invoke a commonjs callback when given a sub-module of an ignored module',  assertResult('moduleC/sub-module', 'commonjs moduleC/sub-module'));
 
-    it('should invoke an empty callback when given an ignored module path',      assertResult('moduleC', undefined));
-    it('should invoke an empty callback when given an ignored sub-module path',  assertResult('moduleA/sub-module', undefined));
-    it('should invoke an empty callback when given an ignored file path',        assertResult('moduleA/another-sub/index.js', undefined));
-    it('should invoke an empty callback when given an ignored regex path',       assertResult('moduleD', undefined));
-    it('should invoke an empty callback when given an ignored regex sub-module path',  assertResult('moduleD/sub-module', undefined));
-    it('should invoke an empty callback when given a non-node module',           assertResult('non-node-module', undefined));
-    it('should invoke an empty callback when given a relative path',             assertResult('./src/index.js', undefined));
+    describe('should invoke a commonjs callback', function(){
+        it('when given an existing module', assertResult('moduleB', 'commonjs moduleB'));
+        it('when given an existing sub-module', assertResult('moduleB/sub-module', 'commonjs moduleB/sub-module'));
+        it('when given a module which is the parent on an ignored path', assertResult('moduleA', 'commonjs moduleA'));
+        it('when given a sub-module of an ignored module', assertResult('moduleC/sub-module', 'commonjs moduleC/sub-module'));
+    });
+
+    describe('should invoke an empty callback', function(){
+        it('when given an ignored module path', assertResult('moduleC', undefined));
+        it('when given an ignored sub-module path', assertResult('moduleA/sub-module', undefined));
+        it('when given an ignored file path', assertResult('moduleA/another-sub/index.js', undefined));
+        it('when given an ignored regex path', assertResult('moduleD', undefined));
+        it('when given an ignored regex sub-module path', assertResult('moduleD/sub-module', undefined));
+        it('when given a non-node module', assertResult('non-node-module', undefined));
+        it('when given a relative path', assertResult('./src/index.js', undefined));
+    });
+
     after(function(){
         restoreMock()
     });
